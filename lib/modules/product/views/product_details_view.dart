1d99025b15b9745a8911f8b589df56f9db3d81f4
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-10-02 09:57:27 +0300

    feat: adds item to cart

--- lib/modules/product/views/product_details_view.dart
+++ lib/modules/product/views/product_details_view.dart
@@ -4,7 +4,7 @@ import 'package:lazy_techno/modules/product/controllers/cart_controller.dart';
 import 'package:lazy_techno/modules/product/models/product_model.dart';
 
 class ProductDetailsView extends StatelessWidget {
-  final CartController cartController = Get.find<CartController>();
+  final CartController cartController = Get.put(CartController());
   // const ProductDetailsView({super.key});
 
   @override
