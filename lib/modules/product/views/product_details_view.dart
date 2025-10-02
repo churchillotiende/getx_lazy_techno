de5ff705d1db5aeb8d7ae79d18076ca28d05e37a
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-10-02 09:43:49 +0300

    uses the cart values on the product

--- lib/modules/product/views/product_details_view.dart
+++ lib/modules/product/views/product_details_view.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lazy_techno/modules/product/controllers/cart_controller.dart';
 import 'package:lazy_techno/modules/product/models/product_model.dart';
 
 class ProductDetailsView extends StatelessWidget {
-  const ProductDetailsView({super.key});
+  final CartController cartController = Get.find<CartController>();
+  // const ProductDetailsView({super.key});
 
   @override
   Widget build(BuildContext context) {
     final ProductModel product = Get.arguments;
 
     return Scaffold(
-      appBar: AppBar(title: Text('Product Details')),
-
+      appBar: AppBar(
+        title: Text('Product Details'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Get.toNamed('/cart');
+              },
+              icon: Icon(Icons.shopping_cart))
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -19,6 +29,13 @@ class ProductDetailsView extends StatelessWidget {
             Text(product.name, style: TextStyle(fontSize: 24)),
             SizedBox(height: 10),
             Text("\$${product.price}", style: TextStyle(fontSize: 24)),
+            ElevatedButton(
+                onPressed: () {
+                  cartController.addToCart(product);
+                  Get.snackbar(
+                      'Added to cart', '${product.name} added to your cart');
+                },
+                child: Text('Add to cart'))
           ],
         ),
       ),
