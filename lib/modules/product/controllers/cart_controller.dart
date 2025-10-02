da3c77fa0be340d17fa4da5ef77d2687058cc910
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-10-02 08:37:11 +0300

    feat: creates add and remove from cart functions

--- lib/modules/product/controllers/cart_controller.dart
+++ lib/modules/product/controllers/cart_controller.dart
@@ -4,4 +4,12 @@ import 'package:lazy_techno/modules/product/models/product_model.dart';
 class CartController extends GetxController {
   var cartItems = <ProductModel>[].obs;
   var totalAmount = 0.0.obs;
+
+  void addToCart(ProductModel product) {
+    cartItems.add(product);
+  }
+
+  void removeFromCart(ProductModel product) {
+    cartItems.add(product);
+  }
 }
