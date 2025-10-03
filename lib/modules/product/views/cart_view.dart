eb449d41239cc1bebc7c1288c46fec69ed704485
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-10-03 15:29:54 +0300

    feat: adds a dialogue box before removing from cart

--- lib/modules/product/views/cart_view.dart
+++ lib/modules/product/views/cart_view.dart
@@ -11,36 +11,54 @@ class CartView extends StatelessWidget {
       appBar: AppBar(title: Text('Cart')),
       body: Column(
         children: [
-          Expanded(child: Obx(() {
-            return ListView.builder(
-              itemCount: cartController.cartItems.length,
-              itemBuilder: (context, index) {
-                final item = cartController.cartItems[index];
-                return ListTile(
-                  title: Text(item.name),
-                  subtitle: Text('\$${item.price.toString()}'),
-                  trailing: IconButton(
-                      onPressed: () => cartController.removeFromCart(item),
-                      icon: Icon(Icons.remove_circle_outline)),
-                );
-              },
-            );
-          })),
+          Expanded(
+            child: Obx(() {
+              return ListView.builder(
+                itemCount: cartController.cartItems.length,
+                itemBuilder: (context, index) {
+                  final item = cartController.cartItems[index];
+                  return ListTile(
+                    title: Text(item.name),
+                    subtitle: Text('\$${item.price.toString()}'),
+                    trailing: IconButton(
+                      icon: Icon(Icons.remove_circle_outline),
+                      onPressed: () {
+                        Get.defaultDialog(
+                          title: "Remove Item",
+                          middleText:
+                              "Are you sure you want to remove ${item.name} from the cart?",
+                          textConfirm: "Yes",
+                          textCancel: "No",
+                          confirmTextColor: Colors.white,
+                          onConfirm: () {
+                            cartController.removeFromCart(item);
+                            Get.back();
+                          },
+                          onCancel: () {},
+                        );
+                      },
+                    ),
+                  );
+                },
+              );
+            }),
+          ),
           Obx(() {
             return Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(
-                'Total:\$${cartController.totalAmount.value.toStringAsFixed(2)}',
+                'Total: \$${cartController.totalAmount.value.toStringAsFixed(2)}',
                 style: TextStyle(fontSize: 24),
               ),
             );
           }),
           ElevatedButton(
-              onPressed: () {
-                Get.toNamed('/checkout');
-                print("Checkout button has been pressed");
-              },
-              child: Text("Proceed to checkout"))
+            onPressed: () {
+              Get.toNamed('/checkout');
+              print("Checkout button has been pressed");
+            },
+            child: Text("Proceed to checkout"),
+          ),
         ],
       ),
     );
